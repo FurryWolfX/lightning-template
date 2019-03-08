757630e4904f732a655ed4e095a029d845d8d1bd
wolfx <digimonx@qq.com>
2019-03-09 00:17:47 +0800

    通用JSON返回模型

--- router/index.js
+++ router/index.js
@@ -1,10 +1,16 @@
 import Lightning from "@wolfx/lightning";
 import { findByAge } from "../service/user";
+import ResultJSON from "../model/ResultJSON";
 
-const app = Lightning.core.getState().app;
+const { app } = Lightning.core.getState();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/test", async (req, res) => {
-  res.send(await findByAge());
+  const result = await findByAge(18);
+  const json = new ResultJSON();
+  json.data = result;
+  json.msg = "查询成功";
+  json.success = true;
+  res.send(json);
 });
