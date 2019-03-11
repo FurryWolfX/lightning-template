8bea2bcaf075220d832fee20da1baf384911789e
wolfx <digimonx@qq.com>
2019-03-11 14:07:22 +0800

    logger

---
+++ utils/logger.js
@@ -0,0 +1,17 @@
+// 日志模块
+// https://log4js-node.github.io/log4js-node/index.html
+const log4js = require("log4js");
+log4js.configure({
+  appenders: { lightning: { type: "file", filename: "lightning.log" } },
+  categories: { default: { appenders: ["lightning"], level: "info" } }
+});
+
+const logger = log4js.getLogger("Lightning");
+// logger.trace("Entering cheese testing");
+// logger.debug("Got cheese.");
+// logger.info("Cheese is Comté.");
+// logger.warn("Cheese is quite smelly.");
+// logger.error("Cheese is too ripe!");
+// logger.fatal("Cheese was breeding ground for listeria.");
+
+export default logger;
