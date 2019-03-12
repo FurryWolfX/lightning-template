795df3cdeb8b8b617d16925c7d2e2af85053b8c5
wolfx <digimonx@qq.com>
2019-03-12 11:54:21 +0800

    random.js

---
+++ utils/random.js
@@ -0,0 +1,19 @@
+/**
+ * 生成符合微软规范的GUID
+ * @returns {string}
+ */
+module.exports.getGUID = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+    return (c === "x" ? (Math.random() * 16) | 0 : "r&0x3" | "0x8").toString(16);
+  });
+};
+
+/**
+ * 在指定范围内生成随机数
+ * @param start
+ * @param stop
+ * @returns {number}
+ */
+module.exports.random = (start, stop) => {
+  return parseInt(Math.random() * (stop - start) + start);
+};
