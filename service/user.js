8abe69855914cca2a1b485605aeb95d18c66faa1
wolfx <digimonx@qq.com>
2019-03-20 14:10:29 +0800

    queryByParams page

--- service/user.js
+++ service/user.js
@@ -10,9 +10,22 @@ module.exports.findByAge = async age => {
 };
 
 module.exports.queryByParams = async params => {
-  params.limitStart = (params.pageIndex - 1) * params.pageSize;
-  params.limitSize = +params.pageSize;
-  return await database.query("test.queryByParams", params);
+  if (params.pageSize) {
+    params.limitStart = (params.pageIndex - 1) * params.pageSize;
+    params.limitSize = +params.pageSize;
+  }
+  const list = await database.query("test.queryByParams", params);
+  if (params.pageSize) {
+    const count = await database.query("test.countByParams", params);
+    return {
+      list,
+      count: count[0].count,
+      pageIndex: params.pageIndex,
+      pageSize: params.pageSize
+    };
+  } else {
+    return list;
+  }
 };
 
 module.exports.findById = async id => {
