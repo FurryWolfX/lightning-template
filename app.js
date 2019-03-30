e2507b9ba7ff1c42144c19cc4e3c8a133ef9f536
wolfx <digimonx@qq.com>
2019-03-30 13:36:34 +0800

    模板更新

--- app.js
+++ app.js
@@ -10,14 +10,23 @@ Lightning.core.setConfig({
     database: "test",
     user: "root",
     password: "junlian",
+    camelCase: true,
     pool: {
       minSize: 5,
       maxSize: 20,
       connectionLimit: 5
+    },
+    debugCallback: (key, sql, params) => {
+      // 这里可以接入log4js等
+      // 使用debugCallback后debug不会输出log，需要在回调中自己处理
     }
   },
   cors: {
     allowedOrigins: ["*"]
+  },
+  responseLogCallback: (method, url, time) => {
+    // 用于监控请求响应时间
+    console.log(`${method} ${url} ${time}ms`);
   },
   storage: path.resolve(__dirname, "./public/upload"),
   yaml: path.resolve(__dirname, "./yaml"),
