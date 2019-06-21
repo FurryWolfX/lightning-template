814e96d7a4da16f906b4a0e944acec26767f7632
wolfx <digimonx@qq.com>
2019-06-21 21:29:55 +0800

    针对Lightning2的修改

--- app.js
+++ app.js
@@ -2,28 +2,6 @@ const Lightning = require("@wolfx/lightning");
 const path = require("path");
 
 Lightning.setConfig({
-  database: {
-    debug: true,
-    dialect: "mysql",
-    host: "192.168.1.22",
-    port: 3306,
-    database: "test",
-    user: "root",
-    password: "junlian",
-    camelCase: true,
-    pool: {
-      minSize: 5,
-      maxSize: 20,
-      connectionLimit: 5
-    },
-    debugCallback: (key, sql, params) => {
-      // 这里可以接入log4js等
-      // 使用debugCallback后debug不会输出log，需要在回调中自己处理
-      console.log(`[SQL] ${key}`);
-      console.log(`[SQL] ${sql}`);
-      console.log(`[SQL] ${JSON.stringify(params)}`);
-    }
-  },
   websocket: false,
   cors: {
     allowedOrigins: ["*"]
@@ -35,7 +13,6 @@ Lightning.setConfig({
     console.log(`[S] ${method} ${url} ${time}ms`);
   },
   storage: path.resolve(__dirname, "./public/upload"),
-  yaml: path.resolve(__dirname, "./yaml"),
   routerDir: path.resolve(__dirname, "./router")
 });
 Lightning.core.start(3001);
--- utils/LightningCore.js
+++ utils/LightningCore.js
@@ -1,12 +1,38 @@
 /**
  * 用于实现一些公共继承
  */
+const path = require("path");
 const Lightning = require("@wolfx/lightning");
+const NodebatisLite = require("@wolfx/nodebatis-lite");
+
+const databaseConfig = {
+  debug: true,
+  dialect: "mysql",
+  host: "192.168.1.22",
+  port: 3306,
+  database: "test",
+  user: "root",
+  password: "junlian",
+  camelCase: true,
+  pool: {
+    minSize: 5,
+    maxSize: 20,
+    connectionLimit: 5
+  },
+  debugCallback: (key, sql, params) => {
+    // 这里可以接入log4js等
+    // 使用debugCallback后debug不会输出log，需要在回调中自己处理
+    console.log(`[SQL] ${key}`);
+    console.log(`[SQL] ${sql}`);
+    console.log(`[SQL] ${JSON.stringify(params)}`);
+  }
+};
+const db = new NodebatisLite(path.resolve(__dirname, "../yaml"), databaseConfig);
 
 class LightningCore {
   constructor() {
     // https://www.npmjs.com/package/@wolfx/nodebatis-lite
-    this.database = Lightning.core.getState().database;
+    this.database = db;
   }
 }
 
