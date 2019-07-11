2d80ecdfad0fa016e649f574b0beda55311182a4
wolfx <digimonx@qq.com>
2019-07-11 23:55:18 +0800

    统一处理接口异常

--- router/user.js
+++ router/user.js
@@ -1,49 +1,27 @@
 const user = require("../service/user");
-const logger = require("../utils/logger");
 const ResultJSON = require("../model/ResultJSON");
 const { get, post } = require("../utils/routerUtil");
 
 get("/user/test", async (req, res) => {
   const json = new ResultJSON();
-  try {
-    json.data = await user.findByAge(18);
-    json.msg = "查询成功";
-    json.success = true;
-  } catch (e) {
-    json.msg = "查询失败";
-    json.success = false;
-    json.errorMsg = e.stack;
-    logger.error(e.stack);
-  }
+  json.data = await user.findByAge(18);
+  json.msg = "查询成功";
+  json.success = true;
   res.send(json);
 });
 
 post("/user/testPost", async (req, res) => {
   const json = new ResultJSON();
-  try {
-    json.data = req.body;
-    json.msg = "查询成功";
-    json.success = true;
-  } catch (e) {
-    json.msg = "查询失败";
-    json.success = false;
-    json.errorMsg = e.stack;
-    logger.error(e.stack);
-  }
+  json.data = req.body;
+  json.msg = "查询成功";
+  json.success = true;
   res.send(json);
 });
 
 get("/user/queryByParams", async (req, res) => {
   const json = new ResultJSON();
-  try {
-    json.data = await user.queryByParams(req.query);
-    json.msg = "查询成功";
-    json.success = true;
-  } catch (e) {
-    json.msg = "查询失败";
-    json.success = false;
-    json.errorMsg = e.stack;
-    logger.error(e.stack);
-  }
+  json.data = await user.queryByParams(req.query);
+  json.msg = "查询成功";
+  json.success = true;
   res.send(json);
 });
--- utils/routerUtil.js
+++ utils/routerUtil.js
@@ -1,16 +1,51 @@
 const Lightning = require("@wolfx/lightning");
 const { projectName } = require("../const/config");
+const ResultJSON = require("../model/ResultJSON");
+const logger = require("../utils/logger");
 
 const { app } = Lightning.core.getState();
 
 module.exports.get = function(url, fn) {
-  app.get(projectName + url, fn);
+  app.get(projectName + url, async (req, res, next) => {
+    const json = new ResultJSON();
+    try {
+      await fn(req, res, next);
+    } catch (e) {
+      json.msg = "server internal error";
+      json.success = false;
+      json.errorMsg = e.stack;
+      logger.error(e.stack);
+      res.send(json);
+    }
+  });
 };
 
 module.exports.post = function(url, fn) {
-  app.post(projectName + url, fn);
+  app.post(projectName + url, async (req, res, next) => {
+    const json = new ResultJSON();
+    try {
+      await fn(req, res, next);
+    } catch (e) {
+      json.msg = "server internal error";
+      json.success = false;
+      json.errorMsg = e.stack;
+      logger.error(e.stack);
+      res.send(json);
+    }
+  });
 };
 
 module.exports.all = function(url, fn) {
-  app.all(projectName + url, fn);
+  app.all(projectName + url, async (req, res, next) => {
+    const json = new ResultJSON();
+    try {
+      await fn(req, res, next);
+    } catch (e) {
+      json.msg = "server internal error";
+      json.success = false;
+      json.errorMsg = e.stack;
+      logger.error(e.stack);
+      res.send(json);
+    }
+  });
 };
