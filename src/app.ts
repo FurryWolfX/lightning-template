67296de86779f3319fe85f2246d5c2e71b4deaa7
wolfx <digimonx@qq.com>
2019-08-23 14:54:45 +0800

    移除pm2，现在自己实现了多进程管理

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,28 @@
-import * as path from "path";
 import * as cluster from "cluster";
-import Lightning from "@wolfx/lightning";
 import "source-map-support/register";
 import logger from "./utils/logger";
+import startServer from "./server";
 
-Lightning.setConfig({
-  cors: {
-    allowedOrigins: ["*"]
-  },
-  requestLogCallback: (method: string, url: string) => {
-    logger.info(`[request:${process.pid}] ${method} ${url}`);
-  },
-  responseLogCallback: (method: string, url: string, time: string) => {
-    logger.info(`[response:${process.pid}] ${method} ${url} ${time}ms`);
-  },
-  storage: path.resolve(__dirname, "../public/upload"),
-  routerDir: path.resolve(__dirname, "./router")
-});
-Lightning.core.start(3001);
+const processNumber = 1;
+
+if (cluster.isMaster) {
+  for (let i = 0; i < processNumber; i++) {
+    cluster.fork();
+  }
+  cluster.on("death", worker => {
+    logger.error(`[Worker:${worker.pid}] died`);
+    cluster.fork();
+  });
+} else {
+  startServer(3001);
+  // 每分钟输出一次内存
+  setInterval(() => {
+    const mString = Math.floor(process.memoryUsage().rss / 1024 / 1024) + "MB";
+    logger.info(`[Worker:${process.pid} -> memory use] ${mString}`);
+  }, 1000 * 60);
+}
 
 // error handler
 process.on("unhandledRejection", (error: Error) => {
   logger.error(error.stack);
 });
-
-// 每分钟输出一次内存
-setInterval(() => {
-  const processType = cluster.isMaster ? "master" : "worker";
-  const mString = Math.floor(process.memoryUsage().rss / 1024 / 1024) + "MB";
-  logger.info(`[${processType}:${process.pid} -> memory use] ${mString}`);
-}, 1000 * 60);
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import * as path from "path";
+import Lightning from "@wolfx/lightning";
+import logger from "./utils/logger";
+
+function start(port) {
+  Lightning.setConfig({
+    cors: {
+      allowedOrigins: ["*"]
+    },
+    requestLogCallback: (method: string, url: string) => {
+      logger.info(`[request:${process.pid}] ${method} ${url}`);
+    },
+    responseLogCallback: (method: string, url: string, time: string) => {
+      logger.info(`[response:${process.pid}] ${method} ${url} ${time}ms`);
+    },
+    storage: path.resolve(__dirname, "../public/upload"),
+    routerDir: path.resolve(__dirname, "./router")
+  });
+
+  Lightning.core.start(port, ipArray => {
+    ipArray.forEach(ip => console.log(`[Worker:${process.pid}] Server started http://${ip}:${port}!`));
+  });
+}
+
+export default start;
