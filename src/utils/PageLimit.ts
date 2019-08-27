a472b3077ddd4567f203b76bdec8197eabb6f252
wolfx <digimonx@qq.com>
2019-08-27 13:48:45 +0800

    分页工具

--- src/utils/PageLimit.ts
+++ src/utils/PageLimit.ts
@@ -1,8 +1,6 @@
 class PageLimit {
   static getLimit(pageSize: number, pageNo: number): number[] {
-    const limitStart = pageSize * (pageNo - 1);
-    const limitEnd = limitStart + pageSize;
-    return [limitStart, limitEnd];
+    return [pageSize * (pageNo - 1), pageSize];
   }
 }
 
